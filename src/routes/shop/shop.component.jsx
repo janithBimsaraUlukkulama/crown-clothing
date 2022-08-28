37d512ba769f68e1d47bad45358c78cb230421dd
bimsara <jbimsaraulukkulama@outlook.com>
2022-08-28 13:07:42 +0530

    nested routing and category page

--- src/routes/shop/shop.component.jsx
+++ src/routes/shop/shop.component.jsx
@@ -1,28 +1,16 @@
-import {Fragment, useContext} from "react";
+import {Routes, Route} from "react-router-dom";
 
-import {CategoriesContext} from "../../context/categories.context";
-import ProductCard from "../../components/product-card/product-card.component";
+import CategoryPreview from '../categories-preview/categories-preview.component'
+import Category from "../category/category.component";
 
 import './shop.styles.scss';
 
 const Shop = () => {
-    const { categoriesMap } = useContext(CategoriesContext);
-
-    console.log(categoriesMap)
-
     return (
-        <Fragment>
-            {Object.keys(categoriesMap).map((title) => (
-                <Fragment key={title}>
-                    <h2>{title}</h2>
-                    <div className='products-container'>
-                        {categoriesMap[title].map((product) => (
-                            <ProductCard key={product.id} product={product} />
-                        ))}
-                    </div>
-                </Fragment>
-            ))}
-        </Fragment>
+       <Routes>
+           <Route index element={<CategoryPreview/>}/>
+           <Route path=':category' element={<Category/>}/>
+       </Routes>
     );
 };
 
