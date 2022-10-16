3f4538df06f5194b1422704530107ae44c634402
bimsara <jbimsaraulukkulama@outlook.com>
2022-10-16 21:19:51 +0530

    react-persist

--- src/store/store.js
+++ src/store/store.js
@@ -1,4 +1,6 @@
 import { compose, createStore, applyMiddleware } from 'redux';
+import {persistStore, persistReducer} from "redux-persist";
+import storage from 'redux-persist/lib/storage'
 import logger from 'redux-logger';
 // import { loggerMiddleware as logger } from './middlewares/logger';
 
@@ -8,6 +10,16 @@ const middleWares = [process.env.NODE_ENV === 'development' && logger].filter(
   Boolean
 );
 
+const persistConfig = {
+    key:'root',
+    storage,
+    blacklist:['user']
+};
+
+const persitedReducer = persistReducer(persistConfig, rootReducer);
+
 const composedEnhancers = compose(applyMiddleware(...middleWares));
 
-export const store = createStore(rootReducer, undefined, composedEnhancers);
+export const store = createStore(persitedReducer, undefined, composedEnhancers);
+
+export const persistor = persistStore(store);
